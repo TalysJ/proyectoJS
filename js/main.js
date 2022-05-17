374babf2a51a27e8d21a90c29997180ab88c39f5
TalysJavier <talysjavier@gmail.com>
2022-05-16 23:34:55 -0400

    se creo nueva rama de proyecto repuestos, fase beta

--- js/main.js
+++ js/main.js
@@ -1,142 +1,264 @@
-let continua = true
-let saldo = 1000
-let paquete1
-let extra;
-let transferir;
-let numeroTelefono;
-let opcion;
-let servicios;
-let pagos;
+//DECALRÉ VARIABLES NECESARIAS
+let carrito = [];
+let botonRegistro = document.getElementById("button-register");
+let carro = document.getElementById("carrito");
+let divNft = document.getElementById("divBmw");
+let inputBuscador = inputTexto.value;
+let botonVaciar = document.getElementById("vaciar-carrito")
+let divTotal = document.getElementById('total-carrito')
 
 
-
-
-const transferencia = () => {
-    transferir = parseFloat(prompt("Cuanto dinero desea transferir? -> "));
-    if (isNaN(transferir)) {
-        alert("Por favor ingrese un monto válido");
-    }else if (otraLinea)  {
-        alert("Favor ingrese numero de telefono a transferir");
-    }
-}
-function otraLinea () {
-    parseFloat(prompt("Por favor ingrese un numero de telefono"));
-    if (transferir > saldo)  {
-        alert("No tiene esa cantidad de dinero");
-        alert("Por favor ingrese un monto válido");
-    } else if (saldo -= transferir) {
-        alert("Transferencia realizada, Su saldo es de: " + saldo);
-    } 
-}
-const pago = () => {
-    pagos = parseInt(prompt("Ingresá el monto a pagar"));
-    if (isNaN(pagos)) {
-        alert("Ingresá una opción válida");
-    } else if (pagos > saldo) {
-        alert("No cuenta con ese monto, su saldo es de");
-        alert(`Su saldo es de: ${saldo}`);
-    } else {
-        (saldo -= pagos)
-        alert(`Pago realizado correctamente. Gracias por utilizar Talys Celucorp! \n Comprobante n° 1001 \n Su saldo es de ` + (saldo));
-    }
-
-}
-const paquete = () => {
-    paquete1 = parseInt(prompt("Ingresá el monto a pagar"));
-    if (isNaN(paquete1)) {
-        alert("Ingresá una opción válida");
-    } else if (paquete1 > saldo) {
-        alert(`No cuenta con ese monto, Su saldo es de: ${saldo}`);
-    } else {
-        (saldo -= paquete1)
-        alert(`Compra realizada con exito! Gracias por utilizar Talys Celucorp! \n Comprobante n° 1001 \n Su saldo es de ` + (saldo));
-    }
-
-}
-
-while (continua) {
-    alert(".:BIENVENIDO A TALYS CELUCORP:. \n 1. Recargar saldo \n 2. Transferir saldo  \n 3. Consultar saldo \n 4. Comprar paquetes \n 5. Salir ");
-    opcion = parseFloat(prompt("Digite una opción: "));
-
-    if (opcion === 1) {
-        extra = parseFloat(prompt("Cuanto dinero desea ingresar a su linea ->"));
-        if (isNaN(extra)) {
-            alert("Por favor ingrese un número válido");
-        } else {
-            saldo += extra;
-            alert("Gracias por utilizar Talys Celucorp! Su saldo es de:" + saldo);
-        }
-    } else if (opcion === 2) {
-        transferencia();
-        otraLinea();
-    } else if (opcion === 3) {
-        alert("Su saldo es de: " + saldo);
-    } else if (opcion === 4) {
-        numeroTelefono = parseInt(prompt("Por favor ingrese su numero de telefono"));
-        if (isNaN(numeroTelefono)) {
-            alert("telefono inválido");
-        } else {
-            bolsa = parseInt(prompt("¿Que paquete desea comprar \n 1. 100 Gb Datos+ 1000 minutos+ 1000 sms por 500 pesos  \n 2. 1000 Minutos por 300 pesos \n 3. 1000 SMS por 300 pesos \n 4. 100 Gb de Datos por 300 pesos \n 5. Volver"))
-            if (isNaN(servicios) || servicios > 5) {
-                alert("Ingresá una opción válida");
-            } else if (bolsa1 === 1) {
-                paquete();
-            } else if (bolsa2 === 2) {
-                paquete()
-            } else if (bolsa3 === 3) {
-                paquete()
-            } //else if (bolsa === 4) {
-            //     paquete()
-            // }    
-        }
-    } else if (opcion === 5) {
-        alert("Gracias por preferir Talys Celucorp");
-        continua = false;
-    } else {
-        alert("Error opcion inválida");
-    }
-}
-
-
-
-// const otraLinea = () => {
-//     numeroTelefono = parseFloat(prompt("Favor indicar numero de telefono al que se va a transferir ->"));
-//     if (isNaN(numeroTelefono)) {
-//         alert("Favor indicar un numero valido")
-//     } else if (saldo -= (transferir)) {
-//         alert("Su saldo es de: " + saldo);
-//     }
-// }
-
-//Declaracion de variables "Bolsa"
-let nombreBolsa = "";
-let gigasBolsa = "";
-let minutosBolsa = "";
-let smsBolsa = "";
-let precioBolsa = "";
-
-// Creacion de clase "bolsa"
-class bolsa {
-    constructor(nombre, gigas, minutos, sms, precio) {
+//CREE LOS OBJETOS CORRESPONDIENTES A LAS NFT QUE TIENE LA TIENDA
+class Nft {
+    constructor(id, coleccion, nombre, descripcion, precio, divisa, img) {
+        this.id = id;
+        this.coleccion = coleccion;
         this.nombre = nombre;
-        this.gigas = gigas;
-        this.minutos = minutos;
-        this.sms = sms;
+        this.descripcion = descripcion;
         this.precio = precio;
+        this.divisa = divisa;
+        this.img = img;
     }
 }
 
-// Bolsas a elegir
-const bolsa1 = new bolsa("Pequeña", "20 Gigas", "100 minutos", "200 sms", "150 pesos");
-const bolsa2 = new bolsa("Mediana", "50 gigas", "500 minutos", "500 sms", "300 pesos");
-const bolsa3 = new bolsa("Full", "100 gigas", "1000 minutos", "1000 sms", "500 pesos");
+const ape1 = new Nft(
+    1,
+    "BORED APE",
+    "Undead ape",
+    "The Earth in 2136 is a very different place. The greed of humanity has plagued the world for decades. Environmental neglect has resulted in the mass destruction of habitats and diseases thrive in every major city. But the digital world isn’t immune, either. With wars raging in the real world, the digital world is abandoned.",
+    99.0,
+    "ETH",
+    "./assets/images/undead-ape.jpg"
+);
+const ape2 = new Nft(
+    2,
+    "BORED APE",
+    "Mutant ape",
+    "The MUTANT APE  is a Mutant Ape that can only be created by exposing an existing Bored Ape to a vial of MUTANT SERUM or by minting a Mutant Ape in the public sale.",
+    169,
+    "ETH",
+    "./assets/images/mutant-ape.png"
+);
+const ape3 = new Nft(
+    3,
+    "BORED APE",
+    "King ape",
+    "The KING APE  is an Ape that can only be created by exposing an existing Bored Ape to a vial of MUTANT SERUM or by minting a Mutant Ape in the public sale.",
+    240,
+    "ETH",
+    "./assets/images/king-ape.webp"
+);
+const punk1 = new Nft(
+    4,
+    "CRYPTOPUNKS",
+    "Cryptopunk #66",
+    "This NFTs series is dedicated to the characters of the famous Cryptopunk series. Take one of the characters to your collection.",
+    6,
+    "ETH",
+    "./assets/images/cryptopunk33.png"
+);
+const punk2 = new Nft(
+    5,
+    "CRYPTOPUNKS",
+    "Cryptopunk #33",
+    "This NFTs series is dedicated to the characters of the famous Cryptopunk series. Take one of the characters to your collection.",
+    0.025,
+    "ETH",
+    "./assets/images/cryptopunk66.png"
+);
+const punk3 = new Nft(
+    6,
+    "CRYPTOPUNKS",
+    "Cryptopunk #70",
+    "This NFTs series is dedicated to the characters of the famous Cryptopunk series. Take one of the characters to your collection.",
+    3.75,
+    "ETH",
+    "./assets/images/cryptopunk70.png"
+);
+const alliepunk1 = new Nft(
+    7,
+    "CRYPTOALLIENS",
+    "Alliepunk #60",
+    "This NFTs series is dedicated to the characters of the famous Cryptopunk series. Take one of the characters to your collection.",
+    7,
+    "ETH",
+    "./assets/images/allienpunk1.jpg"
+);
+const alliepunk2 = new Nft(
+    8,
+    "CRYPTOALLIENS",
+    "Alliepunk #48",
+    "This NFTs series is dedicated to the characters of the famous Cryptopunk series. Take one of the characters to your collection.",
+    2.34,
+    "ETH",
+    "./assets/images/allienpunk2.jpg"
+);
+const alliepunk3 = new Nft(
+    9,
+    "CRYPTOALLIENS",
+    "Alliepunk #103",
+    "This NFTs series is dedicated to the characters of the famous Cryptopunk series. Take one of the characters to your collection.",
+    120,
+    "ETH",
+    "./assets/images/allienpunk3.jpg"
+);
+
+//CREE EL ARRAY QUE CONTIENE TODOS LOS NFTS
+let nfts = [
+    ape1,
+    ape2,
+    ape3,
+    punk1,
+    punk2,
+    punk3,
+    alliepunk1,
+    alliepunk2,
+    alliepunk3,
+];
 
 
-const comprarBolsa = () => {
-    // Array de las bolsas
-    let arrayBolsas = [bolsa1, bolsa2, bolsa3];
-    arrayBolsas.push(bolsa);
-    return arrayBolsas
+//FUNCIÓN PARA MOSTRAR Y CERRAR EL MODAL DE REGISTRO
+const mostrarModal = () => {
+    let modal_container = document.getElementById("modal_container");
+    botonRegistro.addEventListener("click", () => {
+        modal_container.classList.add("show");
+    });
+    const cerrar = document.getElementById("close");
+    cerrar.addEventListener("click", () => {
+        modal_container.classList.remove("show");
+    });
+};
+
+//FUNCION PARA REGISTRAR, GUARDAR Y MOSTRAR EL USUARIO
+const registro = () => {
+    let formulario = document.getElementById("idForm");
+    formulario.addEventListener("submit", (event) => {
+        event.preventDefault();
+        let username = document.getElementById("user").value;
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+
+        let usuario = {
+            username: username,
+            password: password,
+            email: email,
+        };
+        let usuarioJSON = JSON.stringify(usuario);
+        localStorage.setItem("Usuario", usuarioJSON);
+
+        let usuarioParseado = JSON.parse(localStorage.getItem("Usuario"));
+        botonRegistro.remove();
+        let divRegistro = document.getElementById("register");
+        divRegistro.innerHTML += `<p class = "login" > Hola ${usuarioParseado.username} !</p>`;
+
+        console.log(usuarioParseado);
+
+        formulario.reset();
+    });
+};
+
+//FUNCIÓN PARA UTILIZAR EL BUSCADOR
+const buscador = () => {
+    let inputTexto = document.getElementById("inputTexto");
+    inputTexto.addEventListener("change", () => {
+        let buscador = inputTexto.value;
+        let nftFiltrados = nfts.filter((nft) =>
+            nft.coleccion.includes(buscador.toUpperCase())
+        );
+        let divNft = document.getElementById("divNfts");
+        divNft.innerHTML = "";
+        nftFiltrados.forEach((nft) => {
+            const divNftCard = document.createElement("div");
+            divNftCard.classList.add("card");
+            divNftCard.style.width = "18rem";
+
+            const divNftContent = `
+        <div class="card-body">
+        <h5 class="card-title">${nft.nombre}</h5>
+        <img src= "${nft.img}">
+        <p class="card-text">${nft.coleccion}</p>
+        <p class="card-text">${nft.descripcion}</p>
+        <p class="card-text">${nft.precio} ${nft.divisa}</p>
+        <button id="boton${nft.id}" class="btn btn-primary agregar-carrito"> Agregar al carrito</button>
+        </div>
+        `;
+
+            divNftCard.innerHTML = divNftContent;
+            divNftCard
+                .querySelector(".agregar-carrito")
+                .addEventListener("click", () => agregarCarrito(nft));
+            divNft.append(divNftCard);
+        });
+    });
+};
+//FUNCION AGREGAR AL CARRITO
+const agregarCarrito = (nft) => {
+    if (carrito.includes(nft)) {
+        alert("Ya esta en tu carrito!");
+    } else {
+        const divNftCard = document.createElement("div");
+        divNftCard.setAttribute("id", "nft-card");
+        divNftCard.classList.add("card");
+        divNftCard.style.width = "18rem";
+
+        const divNftCardContent = `
+            <div class="card-body">
+                <h5 class="card-title">${nft.nombre}</h5>
+                <p class="card-text">${nft.precio} ${nft.divisa}</p>
+                <button id="eliminar-${nft.id}" class="btn-eliminar">Eliminar</button>
+            </div>`;
+
+        divNftCard.innerHTML = divNftCardContent;
+        divNftCard
+            .querySelector(".btn-eliminar")
+            .addEventListener("click", (e) => eliminarNftDelCarrito(nft, e));
+
+        carro.append(divNftCard);
+        carrito.push(nft);
+
+        if (carrito.length > 0) {
+            let totalCarrito = carrito.reduce(
+                (acc, ite) => acc + ite.precio,
+                0
+            );
+            divTotal.textContent = `Total: ${totalCarrito}`;
+        }
+    }
+
+    localStorage.setItem("NftsAgregados", JSON.stringify(carrito));
+};
+
+
+//FUNCION ELIMINAR DEL CARRITO
+const eliminarNftDelCarrito = (nft, e) => {
+    let nftCard = e.target.closest("#nft-card");
+    for (let c = 0; c < carrito.length; c++) {
+        if (carrito[c] === nft) {
+            carrito.splice(c, 1);
+            nftCard.remove();
+            let totalCarrito = carrito.reduce(
+                (acc, ite) => acc + ite.precio,
+                0
+            );
+            divTotal.textContent = `Total: ${totalCarrito}`;
+        }
+    }
+    console.log(carrito);
+    console.log(carrito.length);
+};
+
+//FUNCION VACIAR CARRITO
+const vaciarCarrito = () => {
+    botonVaciar.addEventListener('click', () => {
+        carro.innerHTML = ""
+        carrito.splice(0, carrito.length);
+        divTotal.textContent = `Total: 0`;
+        console.log(carrito.length)
+    })
 }
 
-comprarBolsa()
+
+mostrarModal();
+registro();
+buscador();
+vaciarCarrito();
--- js/script.js
+++ js/script.js
@@ -1,53 +1,264 @@
-const saludar = () => {
-    let nombre;
-    
-    do  {
-        nombre = prompt ("Bienvenido a Silvestre Petshop, ingrese su nombre para comenzar");
-    } while(nombre === "" || !isNaN(nombre))
+//Variables
+let carrito = [];
+let botonRegistro = document.getElementById("button-register");
+let carro = document.getElementById("carrito");
+let divBmw = document.getElementById("divBmw");
+let inputBuscador = inputTexto.value;
+let botonVaciar = document.getElementById("vaciar-carrito")
+let divTotal = document.getElementById('total-carrito')
 
-    console.log (`Hola ${nombre}`);
+
+//Objetos
+class bmw {
+    constructor(id, serie, nombre, descripcion, precio, divisa, img) {
+        this.id = id;
+        this.serie = serie;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.divisa = divisa;
+        this.img = img;
+    }
+}
+
+const motor1 = new bmw(
+    1,
+    "Motor S85",
+    "Bmw M5 E60 V10 2005",
+    "El BMW S85 es un motor de gasolina V10 de 5.0 litros de altas revoluciones diseñado para utilizar la potencia en una amplia gama de bandas de revoluciones. Su alta relación de compresión de 12.0, superando a los modelos M3 anteriores, puede alcanzar al menos 100 hp / litro mientras alcanza la línea roja a 8250 RPM",
+    3000.0,
+    "USD",
+    "./assets/images/motor-s85-bmw-v10jpg.jpg"
+);
+const motor2 = new bmw(
+    2,
+    "Motor S50",
+    "Bmw M3 E36 1991",
+    "La potencia máxima del motor - 143 kW / 195 hp - se obtiene por 5900 rpm, y el par máximo (torque) de 244 Nm - por 4700 rpm. En 7.30 s el BMW 325i puede acelerar de 0 a 60 mph. El automóvil tiene la capacidad de hacer un cuadro de milla en 15.80 s. El vehículo tiene un coeficiente de resistencia/arrastre de 0.3.",
+    2000,
+    "USD",
+    "./assets/images/motor-S50B32-E36.jpg"
+);
+const motor3 = new bmw(
+    3,
+    "Motor S65",
+    "Motor M3 E92 2007",
+    "El BMW S65 es un motor de gasolina V8 de aspiración natural que se fabricó entre 2007 y 2013. Su uso principal fue en el BMW M3 (donde reemplazó al motor BMW S54 de seis cilindros en línea). No hay un reemplazo directo para el S65, ya que la siguiente generación de M3 cambió a un motor turboalimentado de seis cilindros en línea (el BMW S55 ).",
+    2500,
+    "USD",
+    "./assets/images/Motor-S65-E92.jpg"
+);
+const transmision1 = new bmw(
+    4,
+    "Transmision M54 Zf",
+    "Transmision Bmw M5 E60",
+    "Transmision manual de 6 velocidades para Bmw M5 E60 2005.",
+    1200,
+    "USD",
+    "./assets/images/Transmision-M54-Zf.jpg"
+);
+const transmision2 = new bmw(
+    5,
+    "Transmision 46k",
+    "Transmision de E36",
+    "Transmision de 6 velocidades para Bmw M5 E36 1991.",
+    1000,
+    "USD",
+    "./assets/images/Transmision-46k.jpg"
+);
+const transmision3 = new bmw(
+    6,
+    "Transmision N54",
+    "Transmision para E92",
+    "Transmision de 6 velocidades para Bmw M3 E92 2007.",
+    1200,
+    "USD",
+    "./assets/images/Transmision-N54.jpg"
+);
+const turbo1 = new bmw(
+    7,
+    "Turbo E46",
+    "Turbo E46 para E46 E39",
+    "Turbo garret para modelos 318d 320d 520d E46 E39 2.0D, Diesel.",
+    500,
+    "USD",
+    "./assets/images/turbo-e46.jpg"
+);
+const turbo2 = new bmw(
+    8,
+    "Turbo B58",
+    "Turbo B58 para modelo Bmw 340i",
+    "Turbo B58 para modelo Bmw 340i de 2015 a 2022",
+    700,
+    "USD",
+    "./assets/images/turbo-b58.jpg"
+);
+const turbo3 = new bmw(
+    9,
+    "Turbo F80",
+    "Turbo de Bmw M4",
+    "Turbo para Bmw M2, M3 y M4 F80 F82 F87 2015 a 2022 ",
+    800,
+    "USD",
+    "./assets/images/turbo-f80.jpg"
+);
+
+//Array con todos los repuestos
+let bmws = [
+    motor1,
+    motor2,
+    motor3,
+    transmision1,
+    transmision2,
+    transmision3,
+    turbo1,
+    turbo2,
+    turbo3,
+];
+
+
+//FUNCIÓN PARA MOSTRAR Y CERRAR EL MODAL DE REGISTRO
+const mostrarModal = () => {
+    let modal_container = document.getElementById("modal_container");
+    botonRegistro.addEventListener("click", () => {
+        modal_container.classList.add("show");
+    });
+    const cerrar = document.getElementById("close");
+    cerrar.addEventListener("click", () => {
+        modal_container.classList.remove("show");
+    });
 };
 
-const menuPrincipal = () => {
-    let auxMenu = parseInt (
-        prompt( 
-            `Es un gusto verte por aca, como te podemos ayudar? \n1)Solicitar Catalogo \n2)Ver mis pedidos \n3)Mesa de ayuda`
-        )
-    );
+//FUNCION PARA REGISTRAR, GUARDAR Y MOSTRAR EL USUARIO
+const registro = () => {
+    let formulario = document.getElementById("idForm");
+    formulario.addEventListener("submit", (event) => {
+        event.preventDefault();
+        let username = document.getElementById("user").value;
+        let email = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
 
+        let usuario = {
+            username: username,
+            password: password,
+            email: email,
+        };
+        let usuarioJSON = JSON.stringify(usuario);
+        localStorage.setItem("Usuario", usuarioJSON);
 
-    while (auxMenu >4 || auxMenu < 1){
-    auxMenu = parseInt (
-        prompt( 
-            `Es un gusto verte por aca, selecciona un numero para continuar\n1)Solicitar Catalogo\n2)Ver mis pedidos\n3)Mesa de ayuda`
-        )
-    );
-    }
-    let auxMenu2;
+        let usuarioParseado = JSON.parse(localStorage.getItem("Usuario"));
+        botonRegistro.remove();
+        let divRegistro = document.getElementById("register");
+        divRegistro.innerHTML += `<p class = "login" > Hola ${usuarioParseado.username} !</p>`;
 
-    switch (auxMenu) {
-    case 1:
-        auxMenu2 = "Solicitar Catalogo";
-        break;
-    case 2:
-        auxMenu2 = "Ver mis pedidos";
-        break;
-    case 2:
-        auxMenu2 = "Mesa de ayuda";
-        break;
-    }
+        console.log(usuarioParseado);
 
-    while (auxMenu2 = 1){
-        auxMenu2 = parseInt (
-            prompt( 
-                `Favor escribir correo electronico`
-            )
+        formulario.reset();
+    });
+};
+
+//FUNCIÓN BUSCADOR
+const buscador = () => {
+    let inputTexto = document.getElementById("inputTexto");
+    inputTexto.addEventListener("change", () => {
+        let buscador = inputTexto.value;
+        let bmwFiltrados = bmws.filter((bmw) =>
+            bmw.serie.includes(buscador.toUpperCase())
         );
+        let divBmw = document.getElementById("divBmw");
+        divBmw.innerHTML = "";
+        bmwFiltrados.forEach((bmw) => {
+            const divBmwCard = document.createElement("div");
+            divBmwCard.classList.add("card");
+            divBmwCard.style.width = "18rem";
+
+            const divBmwContent = `
+        <div class="card-body">
+        <h5 class="card-title">${bmw.nombre}</h5>
+        <img src= "${bmw.img}">
+        <p class="card-text">${bmw.serie}</p>
+        <p class="card-text">${bmw.descripcion}</p>
+        <p class="card-text">${bmw.precio} ${bmw.divisa}</p>
+        <button id="boton${bmw.id}" class="btn btn-primary agregar-carrito"> Agregar al carrito</button>
+        </div>
+        `;
+
+            divBmwCard.innerHTML = divBmwContent;
+            divBmwCard
+                .querySelector(".agregar-carrito")
+                .addEventListener("click", () => agregarCarrito(bmw));
+            divBmw.append(divBmwCard);
+        });
+    });
+};
+//FUNCION AGREGAR AL CARRITO
+const agregarCarrito = (bmw) => {
+    if (carrito.includes(bmw)) {
+        alert("Ya esta en tu carrito!");
+    } else {
+        const divBmwCard = document.createElement("div");
+        divBmwCard.setAttribute("id", "bmw-card");
+        divBmwCard.classList.add("card");
+        divBmwCard.style.width = "18rem";
+
+        const divBmwCardContent = `
+            <div class="card-body">
+                <h5 class="card-title">${bmw.nombre}</h5>
+                <p class="card-text">${bmw.precio} ${bmw.divisa}</p>
+                <button id="eliminar-${bmw.id}" class="btn-eliminar">Eliminar</button>
+            </div>`;
+
+        divBmwCard.innerHTML = divBmwCardContent;
+        divBmwCard
+            .querySelector(".btn-eliminar")
+            .addEventListener("click", (e) => eliminarBmwDelCarrito(bmw, e));
+
+        carro.append(divBmwCard);
+        carrito.push(bmw);
+
+        if (carrito.length > 0) {
+            let totalCarrito = carrito.reduce(
+                (acc, ite) => acc + ite.precio,
+                0
+            );
+            divTotal.textContent = `Total: ${totalCarrito}`;
         }
-        
+    }
 
-
+    localStorage.setItem("BmwsAgregados", JSON.stringify(carrito));
 };
 
-saludar ();
-menuPrincipal();
+
+//FUNCION ELIMINAR DEL CARRITO
+const eliminarBmwDelCarrito = (bmw, e) => {
+    let bmwCard = e.target.closest("#bmw-card");
+    for (let c = 0; c < carrito.length; c++) {
+        if (carrito[c] === bmw) {
+            carrito.splice(c, 1);
+            bmwCard.remove();
+            let totalCarrito = carrito.reduce(
+                (acc, ite) => acc + ite.precio,
+                0
+            );
+            divTotal.textContent = `Total: ${totalCarrito}`;
+        }
+    }
+    console.log(carrito);
+    console.log(carrito.length);
+};
+
+//FUNCION VACIAR CARRITO
+const vaciarCarrito = () => {
+    botonVaciar.addEventListener('click', () => {
+        carro.innerHTML = ""
+        carrito.splice(0, carrito.length);
+        divTotal.textContent = `Total: 0`;
+        console.log(carrito.length)
+    })
+}
+
+
+mostrarModal();
+registro();
+buscador();
+vaciarCarrito();
